15c4cfe726a8f2866255c0a5a11856bf3817c5ee
teryong <dthrcrpz@gmail.com>
2021-11-28 22:40:08 +0800

    fixed reset password email

--- resources/views/mails/reset-password.blade.php
+++ resources/views/mails/reset-password.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="sub-header">
-        Hi AAA,
+        Hi {{ $user->first_name }},
     </div>
     <div class="content">
         <p>It seems like you forgot your password. Click <a href="{{ config('app.website_url') . "/reset-password?token=$token" }}" target="_blank">here</a> to reset it. </p>
